4aae7ff985e32ddb0d7d1d4106c50ff8b4da3b67
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-02-22 22:50:30 +0530

    basic backend done

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, request, jsonify
-import joblib
-from flask_cors import CORS 
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from predictFuncs import * 
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)  
 
+@app.get("/")
+def home():
+    return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    year = int(request.form.get('year'))
+    month = int(request.form.get('month'))
 
+    predictions = {}
+    predictions["temp"] = LTpredict(year, month) 
+    predictions["seaLevel"] = SLpredict(year, month)
+    predictions["co2"] = COpredict(year, month)
+
+    return render_template('output.html', data = predictions)
+    # return f"Predicted Data: Temperature - {predictions['temp']}, CO₂ Level - {predictions['co2']} ppm, sea level - {predictions['seaLevel']}"
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(debug=True, port=5001)
---
+++ predictFuncs.py
@@ -0,0 +1,34 @@
+import pickle
+import pandas as pd
+
+
+def LTpredict(year, month):
+    with open("./models/landTemperature.pkl", "rb") as LTfile:
+        LTmodel = pickle.load(LTfile)
+
+        future_year_df = pd.DataFrame([[year, month]], columns=['Year', 'Month'])
+
+        prediction = LTmodel.predict(future_year_df)
+
+        return prediction[0]
+
+def SLpredict(year, month):
+    with open("./models/seaLevel.pkl", "rb") as SLfile:
+        SLmodel = pickle.load(SLfile)
+        future_year_df = pd.DataFrame([[year, month]], columns=['Year', 'Month'])
+
+        prediction = SLmodel.predict(future_year_df)
+
+        return prediction[0]
+
+def COpredict(year, month):
+    with open("./models/co2.pkl", "rb") as COfile:
+        COmodel = pickle.load(COfile)
+        year = 2023
+        month = 1
+        future_year_df = pd.DataFrame([[year, month]], columns=['year', 'Month'])
+
+        prediction = COmodel.predict(future_year_df)
+
+        return prediction[0]
+    
